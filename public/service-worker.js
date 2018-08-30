f547f9a68742a9e9a1ab682dc153022b8d0f31db
AmirTGitHub <amir.gevari@gmail.com>
2018-08-30 17:04:01 +0100

    improve service workers

---
+++ public/service-worker.js
@@ -0,0 +1,82 @@
+"use strict";
+const catchName = "weatherApp__";
+const filesToCatch = [
+  "/",
+  "style/stylesheet.css",
+  "script/index.js",
+  "icon/Cloudy.png",
+  "icon/Rainy.png",
+  "icon/Sunny.png",
+  "icon/logo_black.png",
+  "./"
+];
+
+self.addEventListener("install", event => {
+  console.log("WORKER: install event in progress.");
+  event.waitUntil(
+    caches.open(catchName).then(function(cache) {
+      return cache.addAll(filesToCatch);
+    })
+    //installation completed
+  );
+});
+
+self.addEventListener("fetch", event => {
+  console.log("WORKER: fetch event in progress.");
+  if (event.request.method !== "GET") {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const networked = fetch(event.request)
+        .then(fetchedFromNetwork, unableToResolve)
+        .catch(unableToResolve);
+      return cached || networked;
+
+      function fetchedFromNetwork(response) {
+        const cacheCopy = response.clone();
+        caches
+          .open(catchName + "pages")
+          .then(function add(cache) {
+            cache.put(event.request, cacheCopy);
+          })
+          .then(function() {
+            console.log(
+              "WORKER: fetch response stored in cache.",
+              event.request.url
+            );
+          });
+        return response;
+      }
+
+      function unableToResolve() {
+        console.log("WORKER: fetch request failed in both cache and network.");
+        return new Response("<h1>Service Unavailable</h1>", {
+          status: 503,
+          statusText: "Service Unavailable",
+          headers: new Headers({
+            "Content-Type": "text/html"
+          })
+        });
+      }
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  console.log("WORKER: activate event in progress.");
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys
+          .filter(key => {
+            return !key.startsWith(catchName);
+          })
+          .map(key => {
+            return caches.delete(key);
+          })
+      );
+    })
+    //service workers completed
+  );
+});
